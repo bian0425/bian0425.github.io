9849b6ef0d242b68fd759f87ccd889445c03605c
bian0425 <17185713873@163.com>
2026-01-01 03:13:34 +0800

    Site updated: 2026-01-01 03:13:32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1767207962726";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1767208282169";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
